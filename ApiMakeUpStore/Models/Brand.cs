397ee208ffcadc5363a7bdd0786a0dbffe584526
karine <karine.machado@commbox.com.br>
2022-11-15 13:47:25 -0300

    implemetation of the models Product and Brand

---
+++ ApiMakeUpStore/Models/Brand.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiMakeUpStore.Models
+{
+    [Table("DEV_BRAND")]
+    public class Brand
+    {
+        [Column("ID_BRAND")]
+        public int Id { get; set; }
+
+        [Column("NAME_BRAND")]
+        public string Name { get; set; }
+
+        [Column("COUNTRY_BRAND")]
+        public string Country { get; set; }
+    }
+}
---
+++ ApiMakeUpStore/Models/Product.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace ApiMakeUpStore.Models
+{
+
+    [Table("DEV_PRODUCT")]
+    public class Product
+    {
+        [Column("ID_PRODUCT")]
+        public int Id { get; set; }
+
+        [Column("NAME_PRODUCT")]
+        public string Name { get; set; }
+
+        [Column("ID_Brand")]
+        public int IdBrand { get; set; }
+
+        [Column("DESCRIPTION_PRODUCT")]
+        public string Description { get; set; }
+
+        [Column("TYPE_PRODUCT")]
+        public string Type { get; set; }
+
+        [Column("BODYPART_PRODUCT")]
+        public string BodyPart { get; set; }
+
+        [Column("PRICE_PRODUCT")]
+        public double Price { get; set; }
+
+        [Column("PHOTO_PRODUCT")]
+        public string Photo { get; set; }
+
+        [JsonIgnore]
+        [ForeignKey("IdBrand")]
+        public Brand? Brand { get; set; }
+
+    }
+}
