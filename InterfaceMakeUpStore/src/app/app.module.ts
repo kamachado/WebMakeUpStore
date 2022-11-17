6600c38ff32fd2a75d71744c0b57b9ceef6a7e85
Karine Cardoso Machado <karine.machado@commbox.com.br>
2022-11-17 11:40:56 -0300

    start creation of form to register a product

--- InterfaceMakeUpStore/src/app/app.module.ts
+++ InterfaceMakeUpStore/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { BaseboardComponent } from './component/baseboard/baseboard.component';
+import { CreateProductComponent } from './component/registration/create-product/create-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BaseboardComponent
+    BaseboardComponent,
+    CreateProductComponent
   ],
   imports: [
     BrowserModule,
